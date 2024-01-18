cb6eabc596d4bbd70b0b673e14e966bb712f4013
JustinConner <ohmlotusphoenix@gmail.com>
2024-01-18 17:01:14 -0600

    updated typing Effect

--- public/js/typingEffect.js
+++ public/js/typingEffect.js
@@ -1,5 +1,5 @@
 
-/*document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const sentences = [
         "Hello World. ",
         "Welcome to my portfolio website!",
@@ -38,59 +38,5 @@
 
     // Start typing effect
     typeSentence(0);
-});*/
-
-document.addEventListener("DOMContentLoaded", function() {
-    const sentences = [
-        "Hello World. ",
-        " ",
-        "Welcome to my portfolio website!",
-        " ",
-        "Download my resume.",
-        " ",
-        "Check out my projects.",
-        " ",
-        "Schedule an interview.",
-        " ",
-        "Hire Me!"
-    ];
-
-    const typingSpeed = 100;
-    const container = document.getElementById("typing-container");
-
-    function typeSentence(index) {
-        if (index < sentences.length) {
-            const newContainer = document.createElement("div");
-            newContainer.classList.add("typing-container");
-            container.appendChild(newContainer);
-
-            typeWords(sentences[index], newContainer, 0);
-            setTimeout(function() {
-                typeSentence(index + 1);
-            }, typingSpeed * sentences[index].length);
-        }
-    }
-
-    function typeWords(words, currentContainer, wordIndex) {
-        if (wordIndex < words.length) {
-            const span = document.createElement("span");
-            span.textContent = words[wordIndex];
-            currentContainer.appendChild(span);
-
-            if (words[wordIndex] === "Download" && words[wordIndex + 1] === "my" && words[wordIndex + 2] === "resume.") {
-                // Check if the current words are "Download my resume."
-                const image = document.createElement("img");
-                image.src = "./images/Check.png"; // Replace with the actual image URL
-                image.alt = "Check Icon"; // Replace with appropriate alt text
-                currentContainer.appendChild(image);
-            }
-
-            setTimeout(function() {
-                typeWords(words, currentContainer, wordIndex + 1);
-            }, typingSpeed);
-        }
-    }
-
-    // Start typing effect
-    typeSentence(0);
 });
+
