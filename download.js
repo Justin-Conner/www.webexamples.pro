7a9f0e05c6ad350517392f4da58e8026b2bf9a50
JustinConner <ohmlotusphoenix@gmail.com>
2024-01-20 12:14:52 -0600

    updated resume filepath in js file

--- download.js
+++ download.js
@@ -3,7 +3,7 @@ function downloadResume() {
     var link = document.createElement('a');
     
     // Set the href attribute to the path of the file to be downloaded
-    link.href = '/resume'; // Update this to the correct path
+    link.href = '/resume.pdf'; // Update this to the correct path
     
     // Set the download attribute with the desired file name
     link.download = 'resume.pdf'; // Update this to the desired file name
