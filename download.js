6bac04aefc114725f880eadda3a8ea790dbae3d6
JustinConner <ohmlotusphoenix@gmail.com>
2024-01-31 12:53:59 -0600

    updated resume link and file

--- download.js
+++ download.js
@@ -3,11 +3,10 @@ function downloadResume() {
     var link = document.createElement('a');
     
     // Set the href attribute to the path of the file to be downloaded
-    link.href = '/resume.pdf';
- // Update this to the correct path
+    link.href = 'https://drive.google.com/file/d/1L08Bi_k2cbF-4zlXwNrOsQWvRxInEx2I/view?usp=sharing'; // Update this to the correct path
     
     // Set the download attribute with the desired file name
-    link.download = 'resume.pdf'; // Update this to the desired file name
+    link.download = 'Resume24.pdf'; // Update this to the desired file name
     
     // Append the link to the document
     document.body.appendChild(link);
